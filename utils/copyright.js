93a066fef320af940060f7c0bc97db02953bfeed
tigertang <oncwnuKL8y-8gp5Vgb3owwYJ1ueg@git.weixin.qq.com>
2024-09-08 17:54:32 +0800

    feat: + 工具版本声明

---
+++ utils/copyright.js
@@ -0,0 +1,11 @@
+
+const intro = '\x1B[38;5;79mL\x1B[39m\x1B[38;5;79me\x1B[39m\x1B[38;5;79mx\x1B[39m\x1B[38;5;79mi\x1B[39m\x1B[38;5;79mn\x1B[39m \x1B[38;5;79mO\x1B[39m\x1B[38;5;79mA\x1B[39m \x1B[38;5;79mF\x1B[39m\x1B[38;5;80mr\x1B[39m\x1B[38;5;116mo\x1B[39m\x1B[38;5;110mn\x1B[39m\x1B[38;5;110mt\x1B[39m\x1B[38;5;110m-\x1B[39m\x1B[38;5;110me\x1B[39m\x1B[38;5;110mn\x1B[39m\x1B[38;5;110md\x1B[39m \x1B[38;5;110mU\x1B[39m\x1B[38;5;110mI\x1B[39m \x1B[38;5;110mF\x1B[39m\x1B[38;5;110mr\x1B[39m\x1B[38;5;111ma\x1B[39m\x1B[38;5;111mm\x1B[39m\x1B[38;5;111me\x1B[39m\x1B[38;5;111mw\x1B[39m\x1B[38;5;111mo\x1B[39m\x1B[38;5;111mr\x1B[39m\x1B[38;5;105mk\x1B[39m';
+
+const logo = `\x1B[38;5;112mL\x1B[39m\x1B[38;5;39me\x1B[39m\x1B[38;5;220mU\x1B[39m\x1B[38;5;204mI\x1B[39m`;
+
+export {
+  intro, logo
+}
+
+
+// console.log(logo,'-', intro)
